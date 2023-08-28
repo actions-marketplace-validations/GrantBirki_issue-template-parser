47e9aba56ad97b74530c27506f98e9ecc6731d7b
GrantBirki <grantbirki@github.com>
2023-08-28 16:06:58 -0600

    improve existing tests

--- test.spec.js
+++ test.spec.js
@@ -31,8 +31,7 @@ it('model example raw string', async () => {
   const expectedOutput = require('./fixtures/model-example/expected.json')
   const expectedOutputJson = JSON.stringify(expectedOutput, null, 2)
 
-  const body =
-    '### Model Name\r\n\r\nmy cool molecule\r\n\r\n### Model Description\r\n\r\nThis is a prediction for a super cool molecule\r\n\r\n### Ersilia ID\r\n\r\neos11aa\r\n\r\n### Publication\r\n\r\nThe following link is just an example:\r\n\r\nwww.example.com\r\n\r\n### Code\r\n\r\n_No response_\r\n\r\n### License\r\n\r\n_No response_'
+  const body = require('./fixtures/model-example/issue').issue.body
 
   const jsonDict = await parse(body)
 
